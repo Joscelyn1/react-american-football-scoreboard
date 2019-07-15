9eb34e03af68e23b54acfc42c449b149dc438eff
Joscelyn <JoscelynRose@gmail.com>
2019-07-15 16:46:57 -0400

    fixed touchdown number

--- src/MainApp.js
+++ src/MainApp.js
@@ -29,12 +29,12 @@ const [awayScore, setAwayScore] = useState(0);
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => setHomeScore(prevCount => prevCount + 6)}>Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={() => setHomeScore(prevCount => prevCount + 7)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(prevCount => prevCount + 3)}>Home Field Goal</button>
 
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => setAwayScore(prevCount => prevCount + 6)}>Away Touchdown</button>
+          <button className="awayButtons__touchdown" onClick={() => setAwayScore(prevCount => prevCount + 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={() => setAwayScore(prevCount => prevCount + 3)}>Away Field Goal</button>
         </div>
       </section>
