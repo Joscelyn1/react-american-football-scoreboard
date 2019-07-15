2722f6d2ab8d3ceff5f2a9a54de442af44daf6a8
Joscelyn <JoscelynRose@gmail.com>
2019-07-15 15:46:46 -0400

    WIP: reorganizing components

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,31 +1,31 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, {useState} from "react";
+import BottomRow from './BottomRow.js';
 import "./App.css";
-import BottomRow from "./BottomRow";
 
-function App() {
+function MainApp() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 const [homeScore, setHomeScore] = useState(0); 
 const [awayScore, setAwayScore] = useState(0);
   return (
     <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
+          <section className="scoreboard">
+    <div className="topRow">
+    <div className="home">
+        <h2 className="home__name">Lions</h2>
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+        {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">{homeScore}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section>
+        <div className="home__score">{homeScore}</div>
+    </div>
+    <div className="timer">00:03</div>
+    <div className="away">
+        <h2 className="away__name">Tigers</h2>
+        <div className="away__score">{awayScore}</div>
+    </div>
+    </div>
+    <BottomRow />
+    </section>
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
@@ -42,4 +42,4 @@ const [awayScore, setAwayScore] = useState(0);
   );
 }
 
-export default App;
+export default MainApp;
---
+++ src/Top.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BottomRow from './BottomRow.js';
+import MainApp from './MainApp.js';
+
+
+function Top() {
+
+
+return(
+
+    )
+
+}
+
